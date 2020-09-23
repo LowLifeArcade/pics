47053f6ce836bde12b26431f85e9ef3903e37479
LowLifeArcade <68655112+LowLifeArcade@users.noreply.github.com>
2020-09-22 21:44:39 -0700

    first

---
+++ src/components/SearchBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onFormSubmit = (event) => {
+    // i will do this a lot
+    event.preventDefault();
+
+    // do do bellow we need an arrow function so it shoots and sticks ".this" to the class
+    console.log(this.state.term)
+  }
+
+  render() {
+    return (
+    <div className="ui segment" >
+      <form onSubmit={this.onFormSubmit} className="ui form" >
+        <div className="field" >
+          <label>Image Search</label>
+          <input type="text"  
+          value={this.state.term}
+          onChange={e => this.setState({ term: e.target.value })} />
+        </div>
+      </form>
+    </div>
+    )
+  }
+}
+
+export default SearchBar;
